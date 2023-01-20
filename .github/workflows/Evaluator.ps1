d7d46f25ab3fa1263c45068c78413e2a3b63708c
sndvenk <60557868+sndvenk@users.noreply.github.com>
2023-01-20 15:29:33 +1100

    Update Evaluator.ps1

--- .github/workflows/Evaluator.ps1
+++ .github/workflows/Evaluator.ps1
@@ -5,3 +5,14 @@ $newBaseline = Get-Content .\new_baseline.json -Raw | ConvertFrom-Json
 Compare-object -ReferenceObject $currentBaseline `
     -DifferenceObject $newBaseline `
     -Property @('ruleId','message','Lastname','filePath','lineNumber')
+    
+if(Compare-object -ReferenceObject $currentBaseline `
+    -DifferenceObject $newBaseline `
+    -Property @('ruleId','message','Lastname','filePath','lineNumber'))
+    
+{
+echo "New code which is written as part of this PR creates additional compatabilities with .Net Core!!"
+echo "Hence this commit cannot be allowed!"
+echo "Please check the output of the GitAction and rewrite the affected code!!"
+Exit 1
+}
