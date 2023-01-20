c00ee9e670d5365b85213834c429cccc93f4dc7c
sndvenk <60557868+sndvenk@users.noreply.github.com>
2023-01-20 14:41:32 +1100

    Create Evaluator.ps1

---
+++ .github/workflows/Evaluator.ps1
@@ -0,0 +1,8 @@
+$currentBaseline = Get-Content .\current_baseline.json -Raw | ConvertFrom-Json
+$newBaseline = Get-Content .\new_baseline.json -Raw | ConvertFrom-Json
+
+#So we ask PowerShell what the differences are
+Compare-object -ReferenceObject $currentBaseline `
+    -DifferenceObject $newBaseline `
+    -IncludeEqual `
+    -Property @('ruleId','message','Lastname','filePath','lineNumber')
